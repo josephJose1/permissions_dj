8f1c45d4af7a61a58f8ad85197ab5c1c9852a819
josephJose1 <joseph.19211@gmail.com>
2022-12-10 16:27:02 -0500

    add user and permissions

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from inventory.models import Product
+
+# admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    #what user is able to do
+    def get_form(self, request, obj = None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        
+        if not is_superuser:
+            form.base_fields['name'].disabled = True
+        return form
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    web_id = models.CharField(
+        max_length=50, 
+        unique=True
+    )
+    slug = models.SlugField(
+        max_length=255,
+    )
+    name = models.CharField(
+        max_length=255,
+    )
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(
+        default=False,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=True,
+    )
+    update_at = models.DateTimeField(
+        auto_now=True,
+    )
+    
+    # class Meta:
+    #     permissions = (
+    #         ('view_item', 'View Item'),
+    #     )
+    def __str__(self):
+        return self.name
