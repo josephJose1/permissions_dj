6a278ae81d8487f3afdd3827b792315d5dbeefc5
josephJose1 <joseph.19211@gmail.com>
2022-12-20 17:18:28 -0500

    guardian show nothing

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from inventory.models import Product
 from guardian.admin import GuardedModelAdmin
+from guardian.shortcuts import get_objects_for_user
 
 @admin.register(Product)
 class ProductAdmin(GuardedModelAdmin):
@@ -15,7 +16,13 @@ class ProductAdmin(GuardedModelAdmin):
             return True
     
     def get_queryset(self, request):
-        return super().get_queryset(request)
+        if request.user.is_superuser:
+            return super().get_queryset(request)
+        
+        data = self.get_model_objects(request)
+        return data    
+        
+    
     #create usrer to determine what user has permissions to?
     def has_permission(self, request, obj, action):
         opts = self.opts
@@ -25,9 +32,22 @@ class ProductAdmin(GuardedModelAdmin):
         #check wether of user has permissions
         if obj:
             return request.user.has_perm(f'{opts.app_label}.{code_name}', obj)
+        else:
+            return True
+    def get_model_objects(self, request, action=None, klass=None):
+        opts = self.opts
+        actions = [action]
+        klass = klass if klass else opts.model
+        model_name = klass._meta.model_name
+        return get_objects_for_user(user=request.user, perms=[f'{perm}_{model_name}' for perm in actions], klass=klass, any_perm=True)
+    
+    
+    
     def has_view_permission(self, request, obj=None):
-        return True
+        return self.has_permission(request, obj, 'view')
+    
     def has_change_permission(self, request, obj=None):
-        return True
+        return self.has_permission(request, obj, 'change')
+    
     def has_delete_permission(self, request, obj=None):
-        return True
+        return self.has_permission(request, obj, 'delete')
